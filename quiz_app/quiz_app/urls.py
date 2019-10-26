50ba2acc6f3c85a7619ec2f44cb71394b5709719
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-10-26 14:51:38 +0200

    correct app name

--- quiz_app/quiz_app/urls.py
+++ quiz_app/quiz_app/urls.py
@@ -1,4 +1,4 @@
-"""Quiz_app URL Configuration
+"""quiz_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
