5541001eab7fd32ae7dac760253ec90ab837e030
PiotrPawel <pitero1989@o2.pl>
2019-10-27 12:55:41 +0100

    change url from /user/ to /accounts/

--- quiz_app/quiz_app/urls.py
+++ quiz_app/quiz_app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
