9fa2675a419c7a5cc8ec0d5fa782ce5da74c2870
Lukasz_Springer <lukasz.springer@wp.pl>
2019-10-27 12:29:56 +0100

    classProfile

--- quiz_app/users/models.py
+++ quiz_app/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User as DJANGOUSER
+from quiz.models import quiz
+
 
 class User(DJANGOUSER):
     PERMISSIONS = (
@@ -11,3 +13,12 @@ class User(DJANGOUSER):
     name = models.CharField(max_length=30)
     shirt_size = models.CharField(max_length=1, choices=PERMISSIONS)
 
+class Profile(models.Model):
+    avatar = models.ImageField()
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.ForeignKey(User, on_delete=models.CASCADE)
+    join_date = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_quizies = models.ForeignKey(default=False)
+    user_comments = models.ForeignKey(default=False)
+    hyperlink_settings = models.CharField(default=False)
+    solved_quizies = models.BooleanField(default=False)
