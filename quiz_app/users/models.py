07e509a8131c82b7503d8f61c62473553df5051e
PiotrPawel <pitero1989@o2.pl>
2019-11-12 21:39:38 +0100

    Deleted User Model

--- quiz_app/users/models.py
+++ quiz_app/users/models.py
@@ -1,19 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User as DJANGOUSER
+from django.contrib.auth.models import User
 
 
-class User(DJANGOUSER):
-    PERMISSIONS = (
-        ('G', 'Guest'),
-        ('U', 'User'),
-        ('M', 'Mod'),
-        ('A', 'Admin'),
-    )
-    permissions = models.CharField(max_length=1, choices=PERMISSIONS)
-
-# class Profile(models.Model):
-#     avatar = models.ImageField()
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # user_quizies = models.ForeignKey(default=False)
-    # user_comments = models.ForeignKey(default=False)
 
