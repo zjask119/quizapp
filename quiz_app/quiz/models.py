fb1bc388a8cab1fa5595fff5471c3f7da7bce190
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-11-14 11:50:30 +0100

    added correct Quiz model, changed user on defalut in django

--- quiz_app/quiz/models.py
+++ quiz_app/quiz/models.py
@@ -6,7 +6,7 @@ class Quiz(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    rate = models.FloatField(max_length=5, blank=True)
+    rate = models.FloatField(max_length=5, default=0)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self):
