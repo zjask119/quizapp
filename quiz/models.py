4b9f880bf00bca2679852c34c11b48e34cba2a4d
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-11-14 13:59:43 +0100

    added correct Quiz model, changed user on defalut in django

--- quiz/models.py
+++ quiz/models.py
@@ -1,18 +1,14 @@
 from django.db import models
-from users.models import User
+from django.contrib.auth.models import User
 
-class Category(models.Model):
-    category = models.CharField(max_length=20, primary_key=True)
-
-    def __repr__(self):
-        return self.category
 
 class Quiz(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.CharField(max_length=300)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    rate = models.FloatField(max_length=5)
+    rate = models.FloatField(max_length=5, default=0)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return self.name
 
