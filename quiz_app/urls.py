e2718ee46f4fbc6f16bfe6bbe455ad89f234263e
zbyszek <zbyszko.jaskulski@gmail.com>
2019-11-15 15:26:05 +0100

    main page, nav bar

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from main.views import main_view, quiz_list_view
 
 
 urlpatterns = [
@@ -24,5 +25,5 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-
+    path('main/', quiz_list_view),
 ]
