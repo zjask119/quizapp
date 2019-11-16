7a6f0f00845bad69b9b993f42d7ec3f705c886f8
PiotrPawel <pitero1989@o2.pl>
2019-11-16 11:49:03 +0100

    Refactor app tree

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Quiz(models.Model):
     name = models.CharField(max_length=50)
--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from quiz.models import Quiz
+from main.models import Quiz
 from django.views.generic import ListView
+
 
 def quiz_list_view(request):
     quizs_query = Quiz.objects.all()
     return render(request, "list.html", {"quizs": quizs_query})
+
 
 def main_view(request):
     return render(request, "list.html", {})
