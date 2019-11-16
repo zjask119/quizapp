d9576faca52975a1c89ddc44c3c8fca923f39835
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-11-16 14:45:06 +0100

    model Profile

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+from django.db import models
+from django.contrib.auth.models import User
 from main.models import Quiz
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics')
-    quizes = models.ManyToManyField(Quiz)
+    image = models.ImageField(upload_to='media/profile_pics')
     # comments = models.OneToOneField(Comment)   #
 
 
