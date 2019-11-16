89e1df9fa1f448f2bbc0be363fe7d37eb1e6a54a
PiotrPawel <pitero1989@o2.pl>
2019-11-16 12:42:37 +0100

    Added Question model

--- main/models.py
+++ main/models.py
@@ -21,3 +21,9 @@ class Quiz(models.Model):
         return self.name
 
 
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.question_text
