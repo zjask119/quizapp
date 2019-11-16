6dc4c8f1a24123761759122ad7595362256d2373
PiotrPawel <pitero1989@o2.pl>
2019-11-16 13:04:11 +0100

    Added meta class to Category model in order to have correct plural name

--- main/models.py
+++ main/models.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
 
 class Quiz(models.Model):
     name = models.CharField(max_length=50)
