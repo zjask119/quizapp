826b97fca47a9fae060fd6ca9e31181c26165e94
PiotrPawel <pitero1989@o2.pl>
2019-11-16 11:39:37 +0100

    Moved quiz model from quiz app to main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    rate = models.FloatField(max_length=5, default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Quizzes"
+
+    def __str__(self):
+        return self.name
+
+
--- quiz/models.py
+++ quiz/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Quiz(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=300)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    rate = models.FloatField(max_length=5, default=0)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = "Quizzes"
-
-    def __str__(self):
-        return self.name
-
 
 
 
