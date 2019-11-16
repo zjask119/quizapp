69e348633ad1a534f8f0f37be4d9edc739d79998
PiotrPawel <pitero1989@o2.pl>
2019-11-16 12:43:55 +0100

    Added Answer model

--- main/models.py
+++ main/models.py
@@ -27,3 +27,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=250)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return self.answer_text
