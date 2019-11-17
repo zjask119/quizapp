c2f040fb0db51b90ab6c95bcaf547505bfdb7bd2
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-11-17 11:55:24 +0100

    profile pics

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -18,6 +18,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 from main.views import main_view, quiz_list_view, view_about, view_contact
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,5 +29,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', quiz_list_view),
     path('about/', view_about, name='about'),
-    path('contact/', view_contact, name='contact')
+    path('contact/', view_contact, name='contact'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
