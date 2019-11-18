bb199687b2d9962e4dc0771afa13d71f674228d4
PiotrPawel <pitero1989@o2.pl>
2019-11-18 21:26:42 +0100

    Modified image field in Profile model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from main.models import Quiz
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='media/profile_pics')
+    image = models.ImageField(upload_to='profile_pics')
     # comments = models.OneToOneField(Comment)   #
 
 
