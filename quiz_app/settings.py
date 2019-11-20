188fb40d6e5ca63b79a0c77cb53dbd6865647b81
PiotrPawel <pitero1989@o2.pl>
2019-11-20 20:41:15 +0100

    Added paths to password_reset

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -135,3 +135,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # URL to redirect the user to the homepage
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_QUIZAPP_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_QUIZAPP_PASS')
