238a17b461d81deac75c82d6a39716d9bfd20e2e
PiotrPawel <pitero1989@o2.pl>
2019-11-23 20:35:46 +0100

    Added quiz_create view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from main.models import Quiz, Category, Answer
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage
+from .forms import QuizForm
+
+
+def view_about(request):
+    return render(request, "main/about.html", {'title': 'About'})
+
+
+def view_contact(request):
+    return render(request, "main/contact.html", {'title': 'Contact'})
+
+
+def category_view(request):
+    quiz_page = request.GET.get('page_quiz', 1)
+    category_page = request.GET.get('page_category', 1)
+    quizs_query = Quiz.objects.all()
+    paginator = Paginator(quizs_query, 3)
+    categorys_query = Category.objects.all()
+    paginator2 = Paginator(categorys_query, 5)
+    try:
+        quizs = paginator.page(quiz_page)
+        categorys = paginator2.page(category_page)
+    except EmptyPage:
+        quizs = paginator.page(paginator.num_pages)
+        categorys = paginator2.page(paginator2.num_pages)
+    return render(request, "main/list.html", {"categorys": categorys, "quizs": quizs})
+
+
+def category_view(request):
+    quizs_query = Quiz.objects.all()
+    categorys_query = Category.objects.all()
+    return render(request, "main/list.html", {"categorys": categorys_query, "quizs": quizs_query})
 
 
 def quiz_list_view(request):
@@ -40,32 +73,23 @@ def quiz_check(request):
         if ans.is_correct:
             score += 1
 
-    result = float(score/max_score) * 100
-
+    result = float(score / max_score) * 100
     context = {'score': score, 'max_score': max_score, 'result': result}
-
-    return render(request, "main/score.html", context )
-
-
-def view_about(request):
-    return render(request, "main/about.html", {'title': 'About'})
+    return render(request, "main/score.html", context)
 
 
-def view_contact(request):
-    return render(request, "main/contact.html", {'title': 'Contact'})
+@login_required
+def quiz_create(request):
+    if request.method == 'POST':
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            form.instance.author = request.user
+            form.save()
+            messages.success(request, f'Quiz has been added!')
+            return redirect('/')
+    else:
+        form = QuizForm()
+    return render(request, 'main/quiz_form.html', {'form': form})
 
 
-def category_view(request):
-    quiz_page = request.GET.get('page_quiz', 1)
-    category_page = request.GET.get('page_category', 1)
-    quizs_query = Quiz.objects.all()
-    paginator = Paginator(quizs_query, 3)
-    categorys_query = Category.objects.all()
-    paginator2 = Paginator(categorys_query, 5)
-    try:
-        quizs = paginator.page(quiz_page)
-        categorys = paginator2.page(category_page)
-    except EmptyPage:
-        quizs = paginator.page(paginator.num_pages)
-        categorys = paginator2.page(paginator2.num_pages)
-    return render(request, "main/list.html", {"categorys": categorys, "quizs": quizs})
+
