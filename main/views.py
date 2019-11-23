69cf67d658982451a8c20934a928b1380ea88f26
PiotrPawel <pitero1989@o2.pl>
2019-11-23 11:24:30 +0100

    Added quiz_detail view

--- main/views.py
+++ main/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from main.models import Quiz
+from main.models import Quiz, Question, Answer
 from django.views.generic import ListView
 from django.http import Http404
-
-
-
 
 
 def quiz_list_view(request):
     quizs_query = Quiz.objects.all()
     return render(request, "main/list.html", {"quizs": quizs_query})
+
+
+def quiz_detail(request, quiz_pk):
+    try:
+        quiz = Quiz.object.get(pk=quiz_pk)
+    except Quiz.DoesNotExist:
+        raise Http404("Quiz does not exist!")
+
+    questions = Question.objects.filter(quiz=quiz)
+    answers = Answer.objects.filter(question__quiz=quiz)
+
+    context = {"quiz": quiz,
+               "questions": questions,
+               "answers": answers
+               }
+    return render(request, "main/quiz_detail.html", context)
 
 
 def view_about(request):
@@ -19,8 +32,3 @@ def view_about(request):
 
 def view_contact(request):
     return render(request, "main/contact.html", {'title': 'Contact'})
-
-
-
-
-
